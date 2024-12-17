1c9f9291008969d57e442a3fce2d3373eb821767
Cole Nola <cnola1050207@gmail.com>
2024-12-17 14:08:35 -0600

    black jack in chatper 6

--- Chapter 6/BlackJack.java
+++ Chapter 6/BlackJack.java
@@ -1,8 +1,15 @@
+import java.util.Random;
+
 public class BlackJack {
     private int score;
 
     public BlackJack() {
         score = 0;
+    }
+
+    public void addScore() {
+        Random random = new Random();
+        score += random.nextInt(1, 6);
     }
 
     public int getScore() {
--- Chapter 6/BlackJackTester.java
+++ Chapter 6/BlackJackTester.java
@@ -6,30 +6,47 @@ public class BlackJackTester {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        while(true) {
+        int userTotal = 0;
+        int computerTotal = 0;
 
-            int userTotal = 0;
-            int computerTotal = 0;
+        while(userTotal <= 21) {
+            BlackJack user = new BlackJack();
+            BlackJack computer = new BlackJack();
 
-            while(userTotal <= 21) {
-                BlackJack user = new BlackJack();
-                BlackJack computer = new BlackJack();
-
-                for(int i = 0; i < 2; i++) {
-                    user.addScore();
-                    computer.addScore();
-                }
-
-                while (true) { 
-                    System.out.println("Your total: " + user.getScore());
-                    System.out.println("Would you like to hit?");
-                    String hit = input.nextLine();
-                    if(hit.equals("yes")) {
-                        user.addScore();
-                    }
-                }
-
+            for(int i = 0; i < 2; i++) {
+                user.addScore();
+                computer.addScore();
             }
+
+            while (true) { 
+                System.out.println("Your total: " + user.getScore());
+                System.out.println("Would you like to hit?");
+                String hit = input.nextLine();
+                if(hit.equals("yes")) {
+                    user.addScore();
+                } else {
+                    break;
+                }
+                if(user.getScore() > 21) {
+                    System.out.println("Your total: " + user.getScore());
+                    System.out.println("You busted.");
+                    break;
+                }
+            }
+            System.out.println();
+            while(true) {
+                System.out.println("The computer has " + computer.getScore());
+                if(computer.getScore() <= 15) {
+                    System.out.println("Computer decides to hit. ");
+                    computer.addScore();
+                } else {
+                    System.out.println("Computer decides to stay. ");
+                    break;
+                }
+            }
+
+            System.out.println();
+            System.out.println(winner(user, computer) + " wins");
 
             System.out.println("Would you like to test again? ");
             String option = input.nextLine();
@@ -37,5 +54,12 @@ public class BlackJackTester {
                 break;
             }
         }
+    }
+
+    public static String winner(BlackJack user, BlackJack computer) {
+        if(user.getScore() > computer.getScore() && user.getScore() <= 21) {
+            return "User";
+        }
+        return "Computer";
     }
 }

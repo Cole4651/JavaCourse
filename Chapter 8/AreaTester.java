a443120c79acd03d09bc9b562823cc23e2ba320c
Cole Nola <cnola1050207@gmail.com>
2025-04-28 14:06:17 -0500

    I don't know what happened

---
+++ Chapter 8/AreaTester.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class AreaTester {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        while(true) {
+            int choice;
+            
+            while(true){
+                System.out.println("Which would you like to calculate the area of: \n1.) Circle \n2.) Rectangle \n3.) Cylinder");
+                choice = input.nextInt();
+
+                if(choice == 1 || choice == 2 || choice ==3) {
+                    break;
+                }
+                System.out.println("Invalid option, pick 1, 2, or 3. ");
+            }
+
+            switch (choice) {
+                case 1 -> {
+                    System.out.println("Enter radius: ");
+                    double radius = input.nextDouble();
+                    Area.area(radius);
+                }
+                case 2 -> { 
+                    System.out.println("Enter width: ");
+                    double width = input.nextDouble();
+                    System.out.println("Enter height: ");
+                    double height = input.nextDouble();
+                    Area.area(width, height);
+                }
+                default -> {
+                    System.out.println("Enter radius: ");
+                    double cylinderRadius = input.nextDouble();
+                    System.out.println("Enter height: ");
+                    double cylinderHeight = input.nextDouble();
+                    Area.area("cylinder" ,cylinderRadius, cylinderHeight);
+                }
+
+            }
+
+
+            System.out.println();
+            System.out.println("Would you like to test again? ");
+            input.nextLine();
+            String option = input.nextLine();
+            if(option.equals("no")) {
+                break;
+            }
+        }
+    }
+
+}
--- Chapter 8/Geometry.java
+++ Chapter 8/Geometry.java
@@ -1,3 +1,14 @@
-public class Geometry {
-    
+public class Geometry extends GeometryTester {
+
+    public static double circle(double radius) {
+        return radius * radius * Math.PI;
+    }
+
+    public static double rectangle(double height, double width) {
+        return height * width;
+    }
+
+    public static double triangle(double height, double width) {
+        return height * width * 0.5;
+    }
 }

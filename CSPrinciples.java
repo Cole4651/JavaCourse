ece5ac325e9951c2fba38224186385ef7f1f1189
Cole Nola <cnola1050207@gmail.com>
2025-04-29 11:23:55 -0500

    finished 8.9

--- CSPrinciples.java
+++ CSPrinciples.java
@@ -1,12 +1,11 @@
 import java.util.Scanner;
 
-// Compares rainfall of this year to U.S average rainfall
+// Compares rainfall of this year to the Kansas average rainfall per month
 public class CSPrinciples {
     public static final double kansasAverageRainfall = 5.21;
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
-        // Creates array and fills with user data
+        
         double[] array = new double[12];
         for(int i = 0; i < 12; i++) {
             System.out.println("Enter rainfall for month " + (i + 1) + ": ");
@@ -19,9 +18,6 @@ public class CSPrinciples {
 
     }
 
-    // Method that gets the average rainfall 
-    // per month and then compares it to  
-    // average kansas rainfall per month.
     public static String compareRain(double array[]) {
         double total = 0;
         for(int i = 0; i < 12; i++) {
@@ -29,11 +25,11 @@ public class CSPrinciples {
         }
 
         if(total / 12 < kansasAverageRainfall) {
-            return "This year's rainfall was less than the U.S average rainfall. ";
+            return "This year's monthly average rainfall was less than the Kansas monthly average rainfall. ";
         } else if(total / 12 > kansasAverageRainfall) {
-            return "This year's rainfall was more than the U.S average rainfall. ";
+            return "This year's monthly average rainfall was more than the Kansas monthly average rainfall. ";
         } else { 
-            return "This year's rainfall was the same as the U.S average rainfall. ";
+            return "This year's monthly average rainfall was the same as the Kansas monthly average rainfall. ";
         }
     }
 }
--- Chapter 8/Geometry.java
+++ Chapter 8/Geometry.java
@@ -1,4 +1,52 @@
-public class Geometry extends GeometryTester {
+import java.util.Scanner;
+
+public class Geometry {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        while (true) { 
+            int choice = 0;
+            while(true) {
+                System.out.println("Geometry Calculator ");
+                System.out.println("1. Calculate area of a circle ");
+                System.out.println("2. Calculate area of a rectangle ");
+                System.out.println("3. Calculate area of a triangle ");
+    
+                choice = input.nextInt();
+                if(choice >= 1 && choice <= 3) {
+                    break;
+                }
+                System.out.println("Invalid input, try again. \n");
+            }
+            double area = 0;
+            switch(choice) {
+                case 1 -> {
+                    System.out.println("Enter the radius: ");
+                    double radius = input.nextDouble();
+                    System.out.println("Area of circle: " + rounded(circle(radius)));
+                }
+                default -> {
+                    System.out.println("Enter the base: ");
+                    double base = input.nextDouble();
+                    System.out.println("Enter the height: ");
+                    double height = input.nextDouble();
+                    
+                    switch(choice) {
+                        case 2 -> System.out.println("Area of rectangle: " + rounded(rectangle(height, base)));
+                        default -> System.out.println("Area of triangle: " + rounded(triangle(height, base)));
+                    }
+                }
+            }
+
+            input.nextLine();
+            System.out.println();
+            System.out.println("Would you like to test again? ");
+            String option = input.nextLine();
+            if(option.equals("no")) {
+                break;
+            }
+
+        }
+    }
 
     public static double circle(double radius) {
         return radius * radius * Math.PI;
@@ -10,5 +58,10 @@ public class Geometry extends GeometryTester {
 
     public static double triangle(double height, double width) {
         return height * width * 0.5;
+    }
+    
+    public static double rounded(double num) {
+        num = Math.round(num * 100);
+        return num / 100;
     }
 }

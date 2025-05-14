383c70f3e50413c346659f9f67d4ee636a8c925a
Cole Nola <cnola1050207@gmail.com>
2025-05-14 14:55:26 -0500

    done

--- Chapter 2/StringManipulator.java
+++ Chapter 2/StringManipulator.java
@@ -10,7 +10,17 @@ public class StringManipulator
         System.out.println("\nWhat is your favorite city? ");
         String city = input.nextLine();
 
-        System.out.println("\nNumber of characters in the city: " + city.length());
+        int length = city.length();
+        int count = 0;
+        for(int i = 0; i < length; i++) {
+            if(city.charAt(i) == ' '){
+                count++;
+            }
+        }
+        length -= count;
+
+
+        System.out.println("\nNumber of characters in the city: " + length);
         System.out.println("Name of city in all uppercase letters: " + city.toUpperCase());
         System.out.println("Name of city in all lowercase letters: " + city.toLowerCase());
         System.out.println("First character in the name of the city: " + city.charAt(0));
